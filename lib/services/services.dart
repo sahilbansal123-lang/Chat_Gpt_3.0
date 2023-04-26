ab5ef45e9eb56e486ffbcf02bc79070bc7fbe5b0
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-04-26 12:04:38 +0530

    firstcommit

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,3 +1,4 @@
+import 'package:chatgpt_course/widgets/drop_down.dart';
 import 'package:flutter/material.dart';
 import '../constants/constants.dart';
 import '../widgets/text_widget.dart';
@@ -13,12 +14,15 @@ class Services {
           return Padding(
             padding: const EdgeInsets.all(18.0),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Flexible(
                     child: TextWidget(
                   label: "Chosen Model:",
                   fontSize: 16,
                 )),
+                
+                Flexible(flex: 2, child: DropDownWidget()),
               ],
             ),
           );
--- lib/widgets/drop_down.dart
+++ lib/widgets/drop_down.dart
@@ -1,13 +1,27 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:chatgpt_course/constants/constants.dart';
+import 'package:flutter/material.dart';
 
-class DropDownWidget extends StatelessWidget {
+class DropDownWidget extends StatefulWidget {
   const DropDownWidget({super.key});
 
   @override
+  State<DropDownWidget> createState() => _DropDownWidgetState();
+}
+
+class _DropDownWidgetState extends State<DropDownWidget> {
+  String currentModel = "Model1";
+  @override
   Widget build(BuildContext context) {
-    return DropDownWidget(
-      
+    return DropdownButton(
+      dropdownColor: scaffoldBackgroundColor,
+      iconEnabledColor: Colors.white,
+      items: getModelsItem,
+      value: currentModel,
+      onChanged: (value) {
+        setState(() {
+          currentModel = value.toString();
+        });
+      },
     );
   }
 }
